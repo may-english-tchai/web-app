9825db32eec8eceab1124e4b554efd2eee037d24
EddyBough <boughanmieddy@outlook.fr>
2023-05-12 17:12:36 +0200

    kl

--- src/views/Payment.js
+++ src/views/Payment.js
@@ -13,13 +13,18 @@ const Payment = () => {
                     <div id="para">
                         <h3>Récapitulatif - Overview </h3>
                         <p>Nous sommes ravis de vous aider à apprendre l'anglais autour d'un café
-                            lors de notre rencontre confirmée pour :
-                            Lundi 25  Mars 2023 à 14h00
-                            Lundi 25 March 2023 at 2:00 PM
+                            lors de notre rencontre confirmée pour :<br></br>
+                            Lundi 25  Mars 2023 à 14h00<br></br>
+                            Lundi 25 March 2023 at 2:00 PM<br></br>
                             Veuillez confirmer ces informations avant de procéder au paiement</p>
-                            <button>VALIDER</button>
+                        <button>VALIDER</button>
                         <div class="vertical-line"></div>
-                        
+
+                        <div id="divpayment">
+                            
+
+                        </div>
+
                     </div><div style={{ backgroundImage: "url('/img/discussion2.png')" }} id="discussion"></div>
 
                 </div>
