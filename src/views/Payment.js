b3ae8981e6bd4dc26179b7f57f52448bbc12c2d7
EddyBough <boughanmieddy@outlook.fr>
2023-05-15 14:52:40 +0200

    payment

--- src/views/Payment.js
+++ src/views/Payment.js
@@ -19,6 +19,7 @@ const Payment = () => {
                         <div class="vertical-line"></div>
                         
                         
+                        
 
 
                     </div>
