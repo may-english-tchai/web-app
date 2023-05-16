ebbc0c164736ce49786d9abe744f16fa23ede248
EddyBough <boughanmieddy@outlook.fr>
2023-05-16 11:13:18 +0200

    newversion

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Home from "./views/Home";
+
+import React from 'react'
+
+const App = () => {
+  return (
+<Home/>
+  )
+}
+
+export default App
---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="flex flex-col lg:flex-row">
+        <div className="bg-red-500 text-center">COUCOU</div>
+        <div className="bg-red-500 text-center">COUCOU</div>
+        <div className="bg-red-500 text-center">COUCOU</div>
+        <div className="bg-red-500 text-center">COUCOU</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
