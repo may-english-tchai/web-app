caada309f4497ba071338dc22f6c7617151acf5e
EddyBough <boughanmieddy@outlook.fr>
2023-05-17 11:29:55 +0200

    headerimportedanddesigned

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,31 @@
-import { Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
+import "../assets/styles/header.scss";
+import Logo from './Logo';
+
 
 const Header = () => {
     return (
-        <header className="app-header">
-            <nav>
-                <Link to="/" className="my-text-big">Acceuil</Link>
-            </nav>
-        </header>
-    )
-}
+        <div className="navigation">
+            <Logo />
+            <ul>
+                <NavLink to="/" className={(nav)=>(nav.isActive ? "nav-active" : "")}>
+                    <li>Accueil</li>
+                </NavLink>
+                <NavLink to="/concept" className={(nav)=>(nav.isActive ? "nav-active" : "")}>
+                    <li>Concept</li>
+                </NavLink>
+                <NavLink to="/reservation" className={(nav)=>(nav.isActive ? "nav-active" : "")}>
+                    <li>Reservation</li>
+                </NavLink>
+                <NavLink to="/testimony" className={(nav)=>(nav.isActive ? "nav-active" : "")}>
+                    <li>Temoignage</li>
+                </NavLink>
+                <NavLink to="/contact" className={(nav)=>(nav.isActive ? "nav-active" : "")}>
+                    <li>Contact</li>
+                </NavLink>
+            </ul>
+        </div>
+    );
+};
 
 export default Header;
