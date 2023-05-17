c39d6096bb84f83ad62abb3127e6ceb460b7d9f3
EddyBough <boughanmieddy@outlook.fr>
2023-05-17 11:54:04 +0200

    Reservation component designed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Reservation from "./components/Reservation";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
         </Routes>
+        <Reservation/>
       <Footer />
     </>
   );
