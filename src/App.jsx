0e23a8ef2bfd8558240bb4428a758e91cfdd3511
Fahari Hamada Sidi <mkl.devops@gmail.com>
2023-05-17 07:51:07 +0200

    Install tailwind with sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import Home from "./views/Home";
-
-import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-<Home/>
-  )
-}
+    <>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="container app-home">
+      Home Page
+    </div>
+  );
+};
+
+export default Home;
