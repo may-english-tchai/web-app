179a8406b4235c4ac7b0147c422a09a46542047e
EddyBough <boughanmieddy@outlook.fr>
2023-05-19 16:44:33 +0200

    payment integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home";
-import Footer from "./components/Footer";
-import Reservation from "./components/Reservation";
-import Testimony from "./components/Testimony";
-import Concept from "./components/Concept";
-import Join from "./components/Join";
+import Payment from "./components/Payment";
 
 const App = () => {
   return (
     <>
-      
-      <Join/>
-      <Concept/>
         <Routes>
           <Route path='/' element={<Home />} />
-        </Routes>
-        <Reservation/>
-        <Testimony/>
-      <Footer />
+          <Route path='/home' element={<Home />} />
+          <Route path='/payment' element={<Payment />} />
+        </Routes> 
     </>
   );
 };
---
+++ src/components/Payment.jsx
@@ -0,0 +1,41 @@
+import "../assets/styles/payment.scss";
+
+const Payment = () => {
+    return (
+        <div className="Payment">
+            <div id="relative">
+                <div style={{ backgroundColor: "#EDD8A8" }} id="divOne">
+                    <h2><hr></hr>PAIEMENT/PAYMENT<hr></hr></h2>
+                    <div id="para">
+                        <h3>Récapitulatif - Overview </h3>
+                        <p>Nous sommes ravis de vous aider à apprendre l&apos;anglais autour d&apos;un café
+                            lors de notre rencontre confirmée pour :<br></br>
+                            Lundi 25  Mars 2023 à 14h00<br></br>
+                            Lundi 25 March 2023 at 2:00 PM<br></br>
+                            Veuillez confirmer ces informations avant de procéder au paiement</p>
+                            
+                        
+                        <button>VALIDER</button>
+                        <div className="horizontal-line"></div>
+                        <div className="vertical-line"></div>
+                        
+
+                        
+                        
+                        
+
+
+                    </div>
+                    <div style={{ backgroundImage: "url('/img/discussion2.png')" }} id="discussion"></div>
+
+                </div>
+                <div style={{ backgroundImage: "url('/img/pexels.png')" }} id="bg">
+                </div>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default Payment;
