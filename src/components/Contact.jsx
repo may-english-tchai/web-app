c660e1c7bf09c90b04a968c7dd269607052a2863
dhoimirM <dhoimir.mgomri@gmail.com>
2023-05-29 15:59:13 +0200

    prise en compte des remarque faite sur le pull request

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,37 +1,38 @@
 import "../assets/styles/contact.scss";
+import Button from "./elements/Button";
 
 const Contact = () => {
     return (
         <div className='contact'>
-            <div id="contact-container">
-                <div id="title">
+            <div id="contact-container" className="h-screen">
+                <div id="title" className="w-full" >
                     <hr /> <h2>Contact</h2> <hr />
                 </div>
 
                 <div className="contact-box">
-                    <form>
+                    <form method="post">
                         <div className="form-item">
-                            <input type="text" name="sender" required />
-                            <label>Name</label>
+                            <input id="name" type="text" name="sender" required />
+                            <label htmlFor="name">Name</label>
                         </div>
 
                         <div className="form-item">
-                            <input type="text" name="email" required />
-                            <label>Email</label>
+                            <input id="email" type="text" name="email" required />
+                            <label htmlFor="email">Email</label>
                         </div>
 
                         <div className="form-item">
-                            <input type="text" name="sender" required />
-                            <label>Phone</label>
+                            <input id="phone" type="text" name="sender" required />
+                            <label htmlFor="phone">Phone</label>
                         </div>
 
                         <div className="form-item">
-                            <textarea name="message" id="" cols="30" rows="5" required></textarea>
-                            <label>Message</label>
+                            <textarea name="message" id="message" cols="30" rows="5" required></textarea>
+                            <label htmlFor="message">Message</label>
                         </div>
 
                         <div className="form-item">
-                            <button className="submit-btn">Send</button>
+                            <Button type="submit" className="submit-btn">Send</Button>
                         </div>
                     </form>
                 </div>
