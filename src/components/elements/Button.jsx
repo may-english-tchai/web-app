07ae79351d6f304cabe27555be80ed2c0a09d8fb
EddyBough <boughanmieddy@outlook.fr>
2023-06-01 09:58:05 +0200

    latest version of information page 01062323

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,11 +1,26 @@
-/* eslint-disable react/prop-types */
+import { useLocation } from 'react-router-dom';
 
-const Button = ({children, type = 'button', onClick, ...rest}) => {
+const Button = ({ children, type = 'button', onClick, ...rest }) => {
+  const location = useLocation();
+
+  const buttonStyle = {
+    backgroundColor: location.pathname === '/information' ? 'pink' : '',
+  };
+
   return (
-    <button type={type} className="app-button font-medium text-black w-60 h-10 border-gray-400 border rounded-md" onClick={onClick} {...rest}>
-      {children}
+    <button
+      type={type}
+      className="app-button font-medium text-black w-60 h-10 border-gray-400 border rounded-md"
+      style={buttonStyle}
+      onClick={onClick}
+      {...rest}
+    >
+      {"Valider"}
     </button>
   );
-}
+};
 
 export default Button;
+
+
+
