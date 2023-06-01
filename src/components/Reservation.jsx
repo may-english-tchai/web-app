c811f598a89cd986ec133fb68ba7ca6e62acea04
dhoimirM <121858583+dhoimirM@users.noreply.github.com>
2023-06-01 14:03:03 +0200

    ajout du composant title (#65)

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import "../assets/styles/reservation.scss";
 
 const Reservation = () => {
--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+
 const Button = ({children, type = 'button', onClick, ...rest}) => {
   return (
     <button type={type} className="app-button font-medium text-black w-60 h-10 border-gray-400 border rounded-md" onClick={onClick} {...rest}>
