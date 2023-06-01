5a727957a571507c5dcdd794c5cdf91d62ce93e7
dhoimirM <dhoimir.mgomri@gmail.com>
2023-06-01 12:19:59 +0200

    ajout du composant title

---
+++ src/components/elements/Title.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+const Title = ({ text }) => {
+return (
+    <div>
+    <hr style={{ width: "30%",  border: '1px solid', margin: '3rem 1rem' }} />
+        <h2 style={{ fontSize: '1.5em'}}>{text}</h2>
+    <hr style={{ width: "30%", border: '1px solid', margin: '3rem 1rem' }} />
+    </div>
+);
+};
+
+export default Title;
