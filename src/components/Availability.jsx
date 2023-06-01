4b50db61634b29a220ae7a5b4e074d22d7aa890b
dhoimirM <121858583+dhoimirM@users.noreply.github.com>
2023-06-01 09:30:34 +0200

    ajout du container et des cartes de disponibilité (#63)

--- src/components/Availability.jsx
+++ src/components/Availability.jsx
@@ -1,12 +1,31 @@
 import "../assets/styles/availability.scss"
 
+
 const Availability = () => {
     return (
-        <div className="availability w-full h-full relative">
-            <div id="title" className="mt-20 absolute w-full">
-        <hr /> <h2 >MAKE YOUR RESERVATION</h2> <hr />
-    </div>
+    <div className="availability w-full h-full">
+
+            <div id="title" className="w-full mt-20">
+                <hr /> <h2 >MAKE YOUR RESERVATION</h2> <hr />
+            </div>
+
+        <div className="box">
+
+            <div id="scrollable">
+                <div id="sous-titre">
+                    <h4>Quelles sont vos disponibilités ?</h4>
+                </div>
+
+            <div className="card">
+                <p>Date</p>
+                <p>Heure</p>
+                <div className="location"><p>Lieu</p><img src="/img/lieu.png" alt="lieu"/> </div>
+                <p>Langue</p>
+            </div>
+
+            </div>
         </div>
+    </div>
     );
 };
 
