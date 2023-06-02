1f14920319bd53d523ae26e88a6062cba916bad0
EddyBough <121858537+EddyBough@users.noreply.github.com>
2023-06-02 09:55:34 +0200

    information page designed and integrated (#60)
    * information page designed and integrated
    
    * footer integrated and bg color is changing according to the page we are (#58)
    
    * footer integrated and bg color is change according to the page we are
    
    * footer integrated and bg color is change according to the page we are
    
    * brouillon
    
    * latest version of footer component 31/05/2023
    
    * ajout du container et des cartes de disponibilité (#63)
    
    * latest version of information page 01062323
    
    * link for login in header added (#64)
    
    * ajout du composant title (#65)
    
    * information page designed and integrated
    
    * latest version of information page 01062323
    
    * new version of information page
    
    * latest version of information page 01062323
    
    * latest version 1.1 information page
    
    ---------
    
    Co-authored-by: dhoimirM <121858583+dhoimirM@users.noreply.github.com>

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -1,11 +1,22 @@
-/* eslint-disable react/prop-types */
 
-const Button = ({children, type = 'button', onClick, ...rest}) => {
+
+const Button = ({ children, type = 'button', onClick, ...rest }) => {
+  
+
   return (
-    <button type={type} className="app-button font-medium text-black w-60 h-10 border-gray-400 border rounded-md" onClick={onClick} {...rest}>
-      {children}
+    <button
+      type={type}
+      className="app-button font-medium text-black w-60 h-10 border-gray-400 border rounded-md"
+      
+      onClick={onClick}
+      {...rest}
+    >
+      {"Valider"}
     </button>
   );
-}
+};
 
 export default Button;
+
+
+
---
+++ src/pages/InformationPage.jsx
@@ -0,0 +1,12 @@
+
+import Information from "../components/Information";
+
+const InformationPage = () => {
+  return (
+    <>
+      <Information/>
+    </>
+  );
+};
+
+export default InformationPage;
