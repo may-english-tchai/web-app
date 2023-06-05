32cb648fdd2d75828fbfd94200fe6cf06113204a
EddyBough <121858537+EddyBough@users.noreply.github.com>
2023-06-05 13:00:47 +0200

    68 design of login page (#80)
    * login page version 1
    
    * feat: #

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,12 +1,12 @@
 import SignInForm from "../components/SignInForm";
-import SignInButtons from "../components/SignInButtons";
+// import SignInButtons from '../components/SignInButtons';
 import SignUpNoAccount from "../components/SignUpNoAccount";
 
 const LoginPage = () => {
 	return (
 		<>
 			<SignInForm />
-			<SignInButtons />
+			{/* <SignInButtons /> */}
 			<SignUpNoAccount />
 		</>
 	);
