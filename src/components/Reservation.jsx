39e5fcdba49b1f29311c3535e09c2e846aab5c46
dhoimir <dhoimir.mgomri@gmail.com>
2023-06-06 12:15:33 +0200

    feat: #87 correction of the reservation page

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import "../assets/styles/reservation.scss";
 import Button from "./elements/Button";
+import Title from "./elements/Title";
 
 const Reservation = () => {
 	const navigate = useNavigate();
@@ -9,30 +10,30 @@ const Reservation = () => {
 	};
 
 	return (
-		<div id="reservation" className="reservation">
-			<div id="title">
-				<hr /> <h2>Reservation</h2> <hr />
-			</div>
+		<div className="reservation">
+			<div id="#reservation-container" className="h-screen">
+				<Title text="Reservation" textColor="#fd6c9e" hrColor="#fd6c9e" />
 
-			<div>
-				<p>
-					Réservez votre table et découvrez comment maîtriser rapidement
-					l&apos;anglais. <br />
-					Nous vous offrons une expérience inoubliable et des leçons qui vous
-					permettront d&apos;améliorer votre anglais et de le pratiquer avec des
-					personnes qui partagent la même passion. Réservez votre table
-					maintenant et commencez à parler anglais !
-				</p>
+				<div>
+					<p>
+						Réservez votre table et découvrez comment maîtriser rapidement
+						l&apos;anglais. <br />
+						Nous vous offrons une expérience inoubliable et des leçons qui vous
+						permettront d&apos;améliorer votre anglais et de le pratiquer avec
+						des personnes qui partagent la même passion. Réservez votre table
+						maintenant et commencez à parler anglais !
+					</p>
 
-				<div id="reserver">
-					<Button onClick={handleClick} style={{ backGroundColor: "#eee" }}>
-						Reserver
-					</Button>
-					<img
-						id="pictureReservation"
-						src="/img/Reservez.png"
-						alt="conversation"
-					/>
+					<div id="reserver">
+						<Button onClick={handleClick} style={{ backGroundColor: "#eee" }}>
+							Reserver
+						</Button>
+						<img
+							id="pictureReservation"
+							src="/img/Reservez.png"
+							alt="conversation"
+						/>
+					</div>
 				</div>
 			</div>
 		</div>
