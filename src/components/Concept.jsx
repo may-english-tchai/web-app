1b4577f602f52583ec660050bdd1b48b51ee8fc9
EddyBough <121858537+EddyBough@users.noreply.github.com>
2023-06-08 15:02:11 +0200

    95 changes on concept component part2 (#100)
    * feat: #95 changes on concept component part2
    
    * feat: #95 changes on concept component part2

--- src/components/Concept.jsx
+++ src/components/Concept.jsx
@@ -4,24 +4,22 @@ import Title from "./elements/Title";
 const Concept = () => {
 	return (
 		<div id="concept" className="Concept">
-			<div id="concept-container" className="h-screen">
-				<div id="relative">
-					<div id="pinkdiv">
-						<img src="img/cafe1.png" alt="cafe" />
-					</div>
-					<div id="presentation">
-						<Title text="Concept" textColor="#FD6C9E" hrColor="#FD6C9E" />
+			<div id="relative">
+				<div id="pinkdiv">
+					<img src="img/cafe1.png" alt="cafe" />
+				</div>
+				<div id="presentation">
+					<Title text="Concept" textColor="#FD6C9E" hrColor="#FD6C9E" />
 
-						<h4>QU&apos;EST-CE QUE MAY ENGLISH TCHAI ?</h4>
-						<div id="para">
-							<p>
-								Plongez dans l&apos;univers de l&apos;apprentissage de
-								l&apos;anglais autour d&apos;un café ou d&apos;un thé.
-								<br /> Dans un groupe de 3 personnes, apprenez tous les aspects
-								de la langue et discutez en toute confiance.
-								<br /> Une expérience unique, amusante et enrichissante !
-							</p>
-						</div>
+					<h4>QU&apos;EST-CE QUE MAY ENGLISH TCHAI ?</h4>
+					<div id="para">
+						<p>
+							Plongez dans l&apos;univers de l&apos;apprentissage de
+							l&apos;anglais autour d&apos;un café ou d&apos;un thé.
+							<br /> Dans un groupe de 3 personnes, apprenez tous les aspects de
+							la langue et discutez en toute confiance.
+							<br /> Une expérience unique, amusante et enrichissante !
+						</p>
 					</div>
 				</div>
 			</div>
