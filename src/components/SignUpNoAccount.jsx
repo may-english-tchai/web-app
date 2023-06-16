2d287e2fce637bd3f5b32fe5aff9e019ac8c8bf1
dhoimirM <121858583+dhoimirM@users.noreply.github.com>
2023-06-16 17:23:40 +0200

    feat: #75 featsignin add signin page (#132)

--- src/components/SignUpNoAccount.jsx
+++ src/components/SignUpNoAccount.jsx
@@ -4,7 +4,10 @@ const SignUpNoAccount = () => {
 	return (
 		<p className="my-8 text-xs font-light text-center text-gray-700">
 			Si vous n&apos;avez pas de compte,{" "}
-			<Link to="" className="font-medium text-pink-600 hover:underline">
+			<Link
+				to="/register"
+				className="font-medium text-pink-600 hover:underline"
+			>
 				inscrivez-vous
 			</Link>
 		</p>
