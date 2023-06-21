725f386ed340d92b86db9fab54961625eccc6e96
dhoimir <dhoimir.mgomri@gmail.com>
2023-06-21 23:58:28 +0200

    feat: #137 feat handle submit form contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,37 @@
 import "../assets/styles/contact.scss";
 import Button from "./elements/Button";
 import Title from "./elements/Title";
+import { api } from "../store/http";
+import toast, { Toaster } from "react-hot-toast";
+
+const handleSubmit = (e) => {
+	e.preventDefault();
+	const form = e.target;
+	const formData = new FormData(form);
+
+	api
+		.post("/contacts", formData)
+		.then(() => {
+			toast("Votre Message a bien été envoyé \n Your Message has been sent", {
+				icon: "😉",
+				style: {
+					border: "1px solid #2C7A35",
+					padding: "16px",
+					color: "#2C7A35",
+				},
+			});
+		})
+		.catch(() => {
+			toast("Une erreur s'est produite lors de l'envoi du message.", {
+				icon: "🙁",
+				style: {
+					border: "1px solid #C71610",
+					padding: "16px",
+					color: "#C71610",
+				},
+			});
+		});
+};
 
 const Contact = () => {
 	return (
@@ -9,7 +40,7 @@ const Contact = () => {
 				<Title text="Contact" textColor="#FD6C9E" hrColor="#FD6C9E" />
 
 				<div className="contact-box">
-					<form method="post">
+					<form onSubmit={handleSubmit} method="post">
 						<div className="form-item">
 							<input id="name" type="text" name="sender" required />
 							<label htmlFor="name">Name</label>
@@ -27,7 +58,7 @@ const Contact = () => {
 
 						<div className="form-item">
 							<textarea
-								name="message"
+								name="content"
 								id="message"
 								cols="30"
 								rows="5"
@@ -44,6 +75,7 @@ const Contact = () => {
 					</form>
 				</div>
 			</div>
+			<Toaster containerStyle={{ marginTop: "150px" }} reverseOrder={true} />
 		</div>
 	);
 };
