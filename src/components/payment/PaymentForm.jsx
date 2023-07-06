6e33355a71c8fecb5d456db872182725404d4329
dhoimir <dhoimir.mgomri@gmail.com>
2023-07-06 12:00:56 +0200

    #feat: #158 add hundlesubmit on payment page

---
+++ src/components/payment/PaymentForm.jsx
@@ -0,0 +1,31 @@
+import { getAvailability } from "../../store/booking";
+import { api } from "../../store/http";
+import Button from "../elements/Button";
+
+const PaymentForm = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const id_availability = getAvailability().id.replace(
+			"/api/availabilities/",
+			"",
+		);
+		api
+			.post(`/participations/checkout/${id_availability}`, {})
+			.then((response) => {
+				window.location = response.url;
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+
+	return (
+		<form onSubmit={(e) => handleSubmit(e)} action="">
+			<Button style={{ backgroundColor: "EDD8A8" }} type="submit">
+				VALIDER
+			</Button>
+		</form>
+	);
+};
+
+export default PaymentForm;
