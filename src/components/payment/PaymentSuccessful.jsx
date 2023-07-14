12561c305b48becf7d8d909b6189994ba88ca366
dhoimirM <121858583+dhoimirM@users.noreply.github.com>
2023-07-14 23:57:49 +0200

    #feat: #189 add toast in success payment page (#194)

--- src/components/payment/PaymentSuccessful.jsx
+++ src/components/payment/PaymentSuccessful.jsx
@@ -1,6 +1,8 @@
 import Button from "../elements/Button";
 import "../../assets/styles/paymentsuccessful.scss";
 import { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
 
 const PaymentSuccessful = () => {
 	const [isButtonHovered, setIsButtonHovered] = useState(false);
@@ -11,6 +13,19 @@ const PaymentSuccessful = () => {
 
 	const handleButtonMouseLeave = () => {
 		setIsButtonHovered(false);
+	};
+
+	useEffect(() => {
+		const timeout = setTimeout(() => {
+			toast.success("Votre paiement est reussi ! Merci !");
+		}, 1000);
+		return () => {
+			clearTimeout(timeout);
+		};
+	}, []);
+
+	const returnHomePage = () => {
+		window.location.replace("/");
 	};
 
 	const buttonStyle = {
@@ -28,7 +43,19 @@ const PaymentSuccessful = () => {
 	return (
 		<div className="flex h-screen flex-col md:flex-row items-center max-w-screen-xl mx-auto my-auto">
 			<div className="successfulPayment mt-28 flex flex-col items-center text-center space-y-10 md:space-y-10 md:mx-8 md:w-1/2">
-				<h2 className="text-4xl">Paiement réussi</h2>
+				<Toaster
+					toastOptions={{
+						style: {
+							border: "3px solid green",
+							padding: "16px",
+							color: "green",
+							fontStyle: "bold",
+						},
+					}}
+				/>
+				<div className="text-base md:text-3xl">
+					<h2>Paiement Reussi</h2>
+				</div>
 				<div>
 					<p className=" text-base md:text-xl">
 						Nous attendons avec impatience notre prochain cours ensemble. <br />
@@ -46,6 +73,7 @@ const PaymentSuccessful = () => {
 						}
 						onMouseEnter={handleButtonMouseEnter}
 						onMouseLeave={handleButtonMouseLeave}
+						onClick={returnHomePage}
 					>
 						Retour à la page d&apos;accueil
 					</Button>
